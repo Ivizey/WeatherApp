847bade87e32a801124d7da8fc17625ed10ee120
Pavel Bondar <ivizey@gmail.com>
2020-02-09 11:46:13 +0200

    add struct Request

---
+++ WeatherApp/Request.swift
@@ -0,0 +1,16 @@
+//
+//  Request.swift
+//  WeatherApp
+//
+//  Created by Pavel Bondar on 09.02.2020.
+//  Copyright © 2020 Pavel Bondar. All rights reserved.
+//
+
+import Foundation
+
+struct Request {
+    let type: String
+    let query: String
+    let language: String
+    let unit: String
+}
