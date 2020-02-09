4e7add65b047ceba4d3c1e9845e01419bac6cf18
Pavel Bondar <ivizey@gmail.com>
2020-02-09 16:00:21 +0200

    add UI

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -12,28 +12,34 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
-    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var localtimeLabel: UILabel!
+    @IBOutlet weak var descriptionsLabel: UILabel!
+    @IBOutlet weak var windSpeedLabel: UILabel!
+    @IBOutlet weak var windDirLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var feelslikeLabel: UILabel!
+    @IBOutlet weak var visibilityLabel: UILabel!
     private let networkServices = NetworkService()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        searchBar.delegate = self
         getData()
     }
     
     private func getData() {
-        networkServices.fetchWeather(search: "Киев") { [weak self] (weather, error) in
+        networkServices.fetchWeather(search: "Kiev") { [weak self] (weather, error) in
             guard let weather = weather else { return }
             DispatchQueue.main.async {
+                self?.temperatureLabel.text = String(weather.current.temperature) + " ºC"
                 self?.cityLabel.text = weather.location.name
-                self?.temperatureLabel.text = String(weather.current.temperature)
+                self?.localtimeLabel.text = weather.location.localTime
+                self?.descriptionsLabel.text = weather.current.weatherDescriptions.first
+                self?.windSpeedLabel.text = "wind speed: " + String(weather.current.windSpeed)
+                self?.windDirLabel.text = "wind direction: " + weather.current.windDir
+                self?.humidityLabel.text = "humidity: " + String(weather.current.humidity)
+                self?.feelslikeLabel.text = "feelslike: " + String(weather.current.feelslike)
+                self?.visibilityLabel.text = "visibility: " + String(weather.current.visibility)
             }
         }
-    }
-}
-
-extension ViewController: UISearchBarDelegate {
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        
     }
 }
